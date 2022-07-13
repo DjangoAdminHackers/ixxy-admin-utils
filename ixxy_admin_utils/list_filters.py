b1b30f0d3d6ec5408b18b40304d0e48e77b3c80d
Chen Zhe <fruitschen@gmail.com>
2022-07-13 20:36:29 +0800

    upgrade to django4

--- ixxy_admin_utils/list_filters.py
+++ ixxy_admin_utils/list_filters.py
@@ -1,7 +1,7 @@
 import datetime
 from django.contrib.admin import FieldListFilter, DateFieldListFilter
 from django.utils import timezone
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 
 def custom_field_list_filter(title=None):
